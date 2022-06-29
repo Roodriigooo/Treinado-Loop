d24099b780f1698e0e6e90b6d7829dd754211006
Rodrigo Souza Silva <souza.rodrigo11@gmail.com>
2022-06-29 15:34:29 -0300

    Add files via upload

---
+++ Repetição-Em-Lop.py
@@ -0,0 +1,8 @@
+from ast import For, In
+from mailbox import NotEmptyError
+
+clientes = ('😍 Rodrigo','😜 Souza','🤩 Silva')
+
+for nome in clientes:
+
+    print('Olá', nome, 'Seu Pedido Foi Verificado & Está Pronto. ')   
